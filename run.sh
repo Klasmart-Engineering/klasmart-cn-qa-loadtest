91315b1ccebf150829fba6039a78872d7ec64936
raven <raven.zeng@kidsloop.live>
2022-03-29 17:54:51 +0800

    Signed-off-by: raven <raven.zeng@kidsloop.live>

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ for file in `ls ./**/*.js`;do
 k6 run "$file" 2>&1> "./output/`basename $file`-`date`.txt"
 done
 for file in `ls ./*.js`;do
-k6 run "$file" 2>&1> "./output/$file-`date`.txt"
+k6 run "$file" 2>&1> "./output/`basename $file`-`date`.txt"
 done
