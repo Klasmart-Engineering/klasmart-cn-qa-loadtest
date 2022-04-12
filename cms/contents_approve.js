fd3520cffe64e63b6ec7c6b407b9167e3a8a8ed5
jodie.zhang <jodie.zhang@kidsloop.live>
2022-04-12 17:58:42 +0800

    新文件：   cms/contents_approve.js 	新文件：   cms/contents_pending.js 	新文件：   report/learning_summary_assignments_v2.js 	新文件：   report/learning_summary_live_classes_v2.js

---
+++ cms/contents_approve.js
@@ -0,0 +1,28 @@
+import { sleep } from 'k6';
+import http from 'k6/http';
+import * as env from '../utils/env.js';
+import {
+  defaultHeaders,
+  Check,
+  Options
+} from '../utils/common.js';
+
+import { contentsPending } from './contents_pending.js';
+
+export const options = Options;
+
+export default function main() {
+  const response = contentsApprove();
+  return response;
+}
+
+export function contentsApprove() {
+  let content_id = contentsPending();
+  console.log(content_id);
+  let url = `${env.Loadtest_URL}/v1/contents/${content_id}/review/approve?org_id=${env.ORG_ID}`;
+  console.log(url)
+
+  let res = http.put(url, defaultHeaders);
+  console.log(res.body);
+  Check(res);
+}
---
+++ cms/contents_pending.js
@@ -0,0 +1,27 @@
+import { sleep } from 'k6';
+import http from 'k6/http';
+import * as env from '../utils/env.js';
+import {
+  defaultHeaders,
+  Check,
+  Options
+} from '../utils/common.js';
+
+export const options = Options;
+
+export default function main() {
+
+  const response = contentsPending();
+  return response;
+
+}
+export function contentsPending(){
+  let url = env.Loadtest_URL + '/v1/contents_pending?publish_status=pending&submenu=pending&content_type=1%2C2%2C10&order_by=-update_at&page=1&page_size=20&org_id=' + env.ORG_ID;
+//  console.log(url);
+
+  let res = http.get(url, defaultHeaders);
+  Check(res);
+//  console.log(res.body);
+//  console.log(res.json().list[0].id);
+  return res.json().list[0].id;
+}
